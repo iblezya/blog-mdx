59a6fafd7a9a4ff51c5ee43b41be1eb295ff7a5c
Gerson Salas <iblezya@gmail.com>
2020-05-27 21:07:44 -0500

    Add Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { NavWrapper } from "../elements"
+import { graphql, Link, useStaticQuery } from "gatsby"
+
+export const Nav = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      nav: file(relativePath: { eq: "nav.svg" }) {
+        publicURL
+      }
+    }
+  `)
+
+  return (
+    <NavWrapper>
+      <Link to="/">
+        <img src={data.nav.publicURL} alt="Nav" height="60px" />
+      </Link>
+    </NavWrapper>
+  )
+}
