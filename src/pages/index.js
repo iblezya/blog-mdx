ba5b3640639895513fab9504fbbffb566afc42c0
Gerson Salas <iblezya@gmail.com>
2020-05-30 19:49:50 -0500

    Fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 export const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date="Marzo 28,2020"
+          title="Post Card"
+          excerpt="Rastaraton fumaba sus tronchos antes de comer su queso.Era un ritual suyo que los demas ratones no entendian."
+          slug="/test"
+        />
+      </Content>
     </Container>
   )
 }
